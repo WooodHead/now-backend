710f7ebb61fc49473be6d05012486d7f53cd97a7
will h <whoward@meetup.com>
2018-07-02 13:08:16 -0400

    Update typeform link.

--- packages/now-api/src/index.js
+++ packages/now-api/src/index.js
@@ -124,7 +124,7 @@ app.get(
 );
 
 app.get('/invite', (req, res) => {
-  res.redirect('https://meetupinc.typeform.com/to/WkZJW0');
+  res.redirect('https://meetupinc.typeform.com/to/mYMKbB');
 });
 
 app.get('/images/:width(\\d+)x:height(\\d+)/:originalKey(*)', resizer);
