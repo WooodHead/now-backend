0a3e14a4cc57588d1316c5df6daea78fd80921a7
mike castleman <m@mlcastle.net>
2018-07-30 12:03:20 -0400

    Merge pull request #150 from meetup/delay_if_needed_159401907
    refuse to run cronjobs in the 59th second of any minute

--- packages/now-api/src/jobs/index.js
+++ packages/now-api/src/jobs/index.js
@@ -1,5 +1,8 @@
 // @flow
 import type { $Request, $Response, NextFunction } from 'express';
+import { ChronoField, LocalTime } from 'js-joda';
+
+import { promiseDelay } from '../util';
 import rejectExpiredEventInvites from './rejectExpiredEventInvites';
 import sayHello from './sayHello';
 import sendEventReminders from './sendEventReminders';
@@ -10,10 +13,21 @@ const jobs: { [string]: () => Promise<any> } = {
   sendEventReminders,
 };
 
+const waitASecond = () => {
+  const now = LocalTime.now();
+  if (now.get(ChronoField.SECOND_OF_MINUTE) === 59) {
+    const delay = 1000 - now.get(ChronoField.MILLI_OF_SECOND);
+    console.log(`delaying ${delay}ms before running job`);
+    return promiseDelay(delay);
+  }
+  return Promise.resolve();
+};
+
 export default (req: $Request, res: $Response, next: NextFunction) => {
   const { name } = req.params;
   if (Object.prototype.hasOwnProperty.call(jobs, name)) {
-    jobs[name]()
+    waitASecond()
+      .then(() => jobs[name]())
       .then(() => {
         res.sendStatus(200);
       })
