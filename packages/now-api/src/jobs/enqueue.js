15461ccb12fcb3147278dec6f045fcfd5d8c2206
mike castleman <mlc@meetup.com>
2018-08-07 15:29:48 -0400

    learn to use AWS

--- packages/now-api/src/jobs/enqueue.js
+++ packages/now-api/src/jobs/enqueue.js
@@ -5,13 +5,20 @@ import { handleJob } from './dequeue';
 
 const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
-const { SQS_QUEUE_URL } = process.env;
+const { SQS_QUEUE_URL, VERBOSE } = process.env;
 
 const sqsEnqueue = data =>
-  sqs.sendMessage({
-    MessageBody: JSON.stringify(data),
-    QueueUrl: SQS_QUEUE_URL,
-  });
+  sqs
+    .sendMessage({
+      MessageBody: JSON.stringify(data),
+      QueueUrl: SQS_QUEUE_URL,
+    })
+    .promise()
+    .then(response => {
+      if (VERBOSE === 'true') {
+        console.log(response);
+      }
+    }, console.error);
 
 const enqueue: JobRequest => Promise<any> = SQS_QUEUE_URL
   ? sqsEnqueue
