8959d6d1abb1937c950fbba098cbfa11afacb357
mike castleman <m@mlcastle.net>
2018-10-03 15:06:29 -0400

    Merge pull request #237 from meetup/bye_lambda
     teardown cloudwatchlogs-to-splunk lambda

--- packages/now-api/infra/cloudwatchlogs-to-splunk/tf/lambda.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_lambda_function" "now-cloudwatchlogs-to-splunk" {
-  function_name    = "now-cloudwatchlogs-to-splunk"
-  handler          = "index.handler"
-  runtime          = "nodejs4.3"
-  filename         = "src/function.zip"
-  role             = "${aws_iam_role.now-cloudwatchlogs-to-splunk.arn}"
-  source_code_hash = "${base64sha256(file("src/function.zip"))}"
-  timeout          = 120
-}
